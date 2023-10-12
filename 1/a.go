9ae964a144ba6fac37aa24d99fab3bbaccc4aa2d
Erick Gabriel <erickgabrielalves@hotmail.com>
2023-10-11 23:51:19 -0300

    aula 1, entendendo packages

---
+++ 1/a.go
@@ -0,0 +1,8 @@
+// Arquivos no mesmo diretório, precisam
+// ter o mesmo pacote
+
+// Se eu colocar um package diferente ele
+// vai dar erro
+package main
+
+const a = "Hello, World"
---
+++ 1/main.go
@@ -0,0 +1,14 @@
+// Todo arquivo vai ter um package
+// O nome dele vai ser o nome do diretório
+// com exceção do package main
+
+// O package main é o principal, o ponto inicial
+// da aplicação
+package main
+
+// O package main espera que a função main exista
+func main() {
+	// O main.go tem acesso as informações do arquivo
+	// a.go, pois eles estão dentro do mesmo pacote
+	println(a)
+}
