0b8615aba82122b5e56649b065b290098fb29d34
Erick Gabriel <erickgabrielalves@hotmail.com>
2023-10-12 00:00:35 -0300

    aula 3, criando tipos de dados

---
+++ 3/main.go
@@ -0,0 +1,10 @@
+package main
+
+// O Go permite que eu crie um tipo específico de dado
+type ID int
+
+var ident ID = 1 // Criando uma variável com o tipo de dado que nos criamos
+
+func main() {
+	println(ident)
+}
